bf98efd2fc4ba35c124db1c21d8608b31f84d33a
Andrey Albershteyn <aalbersh@redhat.com>
2023-02-02 13:39:17 +0100

    xfs: add xfs_verity header with descriptor name
    Source kernel commit: bf9ea75cff895bdbf146907c0badc7522e0fd6c7
    
    fs-verity defines descriptor name which is used as extended
    attribute name on the file.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>

---
+++ include/xfs_verity.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Red Hat, Inc.
+ */
+#ifndef __XFS_VERITY_H__
+#define __XFS_VERITY_H__
+
+#define XFS_VERITY_DESCRIPTOR_NAME "vdesc"
+#define XFS_VERITY_DESCRIPTOR_NAME_LEN 5
+
+#endif	/* __XFS_VERITY_H__ */
