991b07f50dac636871cf93f8a189dad8556198d3
Andrey Albershteyn <aalbersh@redhat.com>
2024-05-22 19:52:32 +0200

    xfs_io: make ls/chattr work with special files
    With new FS[GET|SET]XATTRAT syscalls we can now list/change inode
    extended attributes on special files instead for ignoring them.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- io/attr.c
+++ io/attr.c
@@ -8,6 +8,7 @@
 #include "input.h"
 #include "init.h"
 #include "io.h"
+#include "libfrog/ixattr.h"
 
 static cmdinfo_t chattr_cmd;
 static cmdinfo_t lsattr_cmd;
@@ -157,24 +158,21 @@ lsattr_callback(
 	struct FTW		*data)
 {
 	struct fsxattr		fsx;
-	int			fd;
+	int			error;
 
 	if (recurse_dir && !S_ISDIR(stat->st_mode))
 		return 0;
 
-	if ((fd = open(path, O_RDONLY)) == -1) {
-		fprintf(stderr, _("%s: cannot open %s: %s\n"),
-			progname, path, strerror(errno));
-		exitcode = 1;
-	} else if ((xfsctl(path, fd, FS_IOC_FSGETXATTR, &fsx)) < 0) {
+	error = getfsxattrat(AT_FDCWD, path, stat, &fsx, AT_SYMLINK_NOFOLLOW);
+	if (error) {
 		fprintf(stderr, _("%s: cannot get flags on %s: %s\n"),
 			progname, path, strerror(errno));
 		exitcode = 1;
-	} else
-		printxattr(fsx.fsx_xflags, 0, 1, path, 0, 1);
+		return 0;
+	}
 
-	if (fd != -1)
-		close(fd);
+	printxattr(fsx.fsx_xflags, 0, 1, path, 0, 1);
+
 	return 0;
 }
 
@@ -186,6 +184,8 @@ lsattr_f(
 	struct fsxattr	fsx;
 	char		*name = file->name;
 	int		c, aflag = 0, vflag = 0;
+	struct stat	st;
+	int		error;
 
 	recurse_all = recurse_dir = 0;
 	while ((c = getopt(argc, argv, "DRav")) != EOF) {
@@ -211,17 +211,27 @@ lsattr_f(
 	if (recurse_all || recurse_dir) {
 		nftw(name, lsattr_callback,
 			100, FTW_PHYS | FTW_MOUNT | FTW_DEPTH);
-	} else if ((xfsctl(name, file->fd, FS_IOC_FSGETXATTR, &fsx)) < 0) {
+		return 0;
+	}
+
+	error = stat(name, &st);
+	if (error)
+		return error;
+
+	error = getfsxattrat(AT_FDCWD, name, &st, &fsx, AT_SYMLINK_NOFOLLOW);
+	if (error) {
 		fprintf(stderr, _("%s: cannot get flags on %s: %s\n"),
 			progname, name, strerror(errno));
 		exitcode = 1;
-	} else {
-		printxattr(fsx.fsx_xflags, vflag, !aflag, name, vflag, !aflag);
-		if (aflag) {
-			fputs("/", stdout);
-			printxattr(-1, 0, 1, name, 0, 1);
-		}
+		return 0;
 	}
+
+	printxattr(fsx.fsx_xflags, vflag, !aflag, name, vflag, !aflag);
+	if (aflag) {
+		fputs("/", stdout);
+		printxattr(-1, 0, 1, name, 0, 1);
+	}
+
 	return 0;
 }
 
@@ -233,31 +243,28 @@ chattr_callback(
 	struct FTW		*data)
 {
 	struct fsxattr		attr;
-	int			fd;
+	int			error;
 
 	if (recurse_dir && !S_ISDIR(stat->st_mode))
 		return 0;
 
-	if ((fd = open(path, O_RDONLY)) == -1) {
-		fprintf(stderr, _("%s: cannot open %s: %s\n"),
-			progname, path, strerror(errno));
-		exitcode = 1;
-	} else if (xfsctl(path, fd, FS_IOC_FSGETXATTR, &attr) < 0) {
+	error = getfsxattrat(AT_FDCWD, path, stat, &attr, AT_SYMLINK_NOFOLLOW);
+	if (error) {
 		fprintf(stderr, _("%s: cannot get flags on %s: %s\n"),
 			progname, path, strerror(errno));
 		exitcode = 1;
-	} else {
-		attr.fsx_xflags |= orflags;
-		attr.fsx_xflags &= ~andflags;
-		if (xfsctl(path, fd, FS_IOC_FSSETXATTR, &attr) < 0) {
-			fprintf(stderr, _("%s: cannot set flags on %s: %s\n"),
-				progname, path, strerror(errno));
-			exitcode = 1;
-		}
+		return 0;
 	}
 
-	if (fd != -1)
-		close(fd);
+	attr.fsx_xflags |= orflags;
+	attr.fsx_xflags &= ~andflags;
+	error = setfsxattrat(AT_FDCWD, path, stat, &attr, AT_SYMLINK_NOFOLLOW);
+	if (error) {
+		fprintf(stderr, _("%s: cannot set flags on %s: %s\n"),
+			progname, path, strerror(errno));
+		exitcode = 1;
+	}
+
 	return 0;
 }
 
@@ -270,6 +277,8 @@ chattr_f(
 	struct xflags	*p;
 	unsigned int	i = 0;
 	char		*c, *name = file->name;
+	struct stat	st;
+	int		error;
 
 	orflags = andflags = 0;
 	recurse_all = recurse_dir = 0;
@@ -326,19 +335,30 @@ chattr_f(
 	if (recurse_all || recurse_dir) {
 		nftw(name, chattr_callback,
 			100, FTW_PHYS | FTW_MOUNT | FTW_DEPTH);
-	} else if (xfsctl(name, file->fd, FS_IOC_FSGETXATTR, &attr) < 0) {
+		return 0;
+	}
+
+	error = stat(name, &st);
+	if (error)
+		return error;
+
+	error = getfsxattrat(AT_FDCWD, name, &st, &attr, AT_SYMLINK_NOFOLLOW);
+	if (error) {
 		fprintf(stderr, _("%s: cannot get flags on %s: %s\n"),
 			progname, name, strerror(errno));
 		exitcode = 1;
-	} else {
-		attr.fsx_xflags |= orflags;
-		attr.fsx_xflags &= ~andflags;
-		if (xfsctl(name, file->fd, FS_IOC_FSSETXATTR, &attr) < 0) {
-			fprintf(stderr, _("%s: cannot set flags on %s: %s\n"),
-				progname, name, strerror(errno));
-			exitcode = 1;
-		}
+		return 0;
 	}
+
+	attr.fsx_xflags |= orflags;
+	attr.fsx_xflags &= ~andflags;
+	error = setfsxattrat(AT_FDCWD, name, &st, &attr, AT_SYMLINK_NOFOLLOW);
+	if (error) {
+		fprintf(stderr, _("%s: cannot set flags on %s: %s\n"),
+			progname, name, strerror(errno));
+		exitcode = 1;
+	}
+
 	return 0;
 }
 
