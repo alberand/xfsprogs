7298b5371fa3e004f22ee7886e9feb134c3e0c00
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:49 -0700

    xfs_spaceman: report data corruption
    Report data corruption to userspace.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- spaceman/health.c
+++ spaceman/health.c
@@ -206,6 +206,10 @@ static const struct flag_map inode_flags[] = {
 		.mask = XFS_BS_SICK_DIRTREE,
 		.descr = "directory tree structure",
 	},
+	{
+		.mask = XFS_BS_SICK_DATA,
+		.descr = "file data",
+	},
 	{0},
 };
 
