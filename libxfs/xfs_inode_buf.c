da9ab97b8cebd4e4a187e1ce053e2aadca3130a0
Andrey Albershteyn <aalbersh@redhat.com>
2024-07-03 14:22:43 -0700

    xfs: add inode on-disk VERITY flag
    Add flag to mark inodes which have fs-verity enabled on them (i.e.
    descriptor exist and tree is built).
    
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_inode_buf.c
+++ libxfs/xfs_inode_buf.c
@@ -723,6 +723,14 @@ xfs_dinode_verify(
 	if ((flags2 & XFS_DIFLAG2_REFLINK) && (flags & XFS_DIFLAG_REALTIME))
 		return __this_address;
 
+	/* only regular files can have fsverity */
+	if (flags2 & XFS_DIFLAG2_VERITY) {
+		if (!xfs_has_verity(mp))
+			return __this_address;
+		if ((mode & S_IFMT) != S_IFREG)
+			return __this_address;
+	}
+
 	/* COW extent size hint validation */
 	fa = xfs_inode_validate_cowextsize(mp, be32_to_cpu(dip->di_cowextsize),
 			mode, flags, flags2);
