6488dabd357f4a7a8537207f49c83fc0feebed00
Andrey Albershteyn <aalbersh@redhat.com>
2025-12-05 16:01:55 +0100

    xfs: convert xfs_log_iovec_t typedef to struct
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- libxlog/xfs_log_recover.c
+++ libxlog/xfs_log_recover.c
@@ -1112,8 +1112,8 @@ xlog_recover_add_to_trans(
 		}
 
 		item->ri_total = in_f->ilf_size;
-		item->ri_buf = kzalloc(item->ri_total * sizeof(xfs_log_iovec_t),
-				    0);
+		item->ri_buf = kzalloc(
+			item->ri_total * sizeof(struct xfs_log_iovec), 0);
 	}
 	ASSERT(item->ri_total > item->ri_cnt);
 	/* Description region is ri_buf[0] */
